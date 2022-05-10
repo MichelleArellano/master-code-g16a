61d176b0204801c12c76670fca88d3092e27b0c7
Michelle Arellano <michelle.arellanofig@gmail.com>
2022-05-10 18:56:08 -0500

    Scope

---
+++ 02-Intro_a_JS/07-Scope/js/app.js
@@ -0,0 +1,31 @@
+// var color = 'negro';
+
+// function pinturas(){
+//     var color = 'rojo';
+//     console.log(color);
+
+//     var listaDeColores = 'Mi colore favorito es: ' + color;
+//     console.log(listaDeColores);
+// }
+
+// console.log(color);
+
+// pinturas(color);
+
+var edadActual = 4;
+
+function calcularDelBebe(){
+    var edadTotal = ('una operacion');
+    var suma = edadTotal + edadActual;
+
+    function dias(){
+        var diasDeNacido = 5;
+        edadActual = diasDeNacido - 8;
+        
+
+    }
+
+    console.log(edadTotal);
+}
+calcularDelBebe();
+console.log(edadTotal);
