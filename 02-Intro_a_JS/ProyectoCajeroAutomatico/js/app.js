5c2612fdab0cb337c6934be6b6076a0578ebba79
Michelle Arellano <michelle.arellanofig@gmail.com>
2022-05-13 15:33:59 -0500

    Validar cuenta

--- 02-Intro_a_JS/ProyectoCajeroAutomatico/js/app.js
+++ 02-Intro_a_JS/ProyectoCajeroAutomatico/js/app.js
@@ -1,35 +1,41 @@
 //Cuentas disponibles
-const cuentas = [
-    {nombre: "Mali", saldo: 200},
-    {nombre: "Gera", saldo: 290},
-    {nombre: "Maui", saldo: 67}
+const accounts = [
+    {name: "Mali", money: 200},
+    {name: "Gera", money: 290},
+    {name: "Maui", money: 67}
 ];
 
 //Asignando nombres en las cards
-for(let i = 0; i < cuentas.length; i++){
-    if(cuentas[i].nombre === "Mali"){
-        document.getElementById('Mali').innerText = cuentas[i].nombre;
-    }else if(cuentas[i].nombre === "Gera"){
-        document.getElementById('Gera').innerText = cuentas[i].nombre;
-    }else if(cuentas[i].nombre === "Maui"){
-        document.getElementById('Maui').innerText = cuentas[i].nombre;
+for(let i = 0; i < accounts.length; i++){
+    if(accounts[i].name === "Mali"){
+        document.getElementById('Mali').innerText = accounts[i].name;
+    }else if(accounts[i].name === "Gera"){
+        document.getElementById('Gera').innerText = accounts[i].name;
+    }else if(accounts[i].name === "Maui"){
+        document.getElementById('Maui').innerText = accounts[i].name;
     }
 }
 
-// Example starter JavaScript for disabling form submissions if there are invalid fields
-(function () {
-    'use strict'
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    var forms = document.querySelectorAll('.needs-validation')
-    // Loop over them and prevent submission
-    Array.prototype.slice.call(forms)
-        .forEach(function (form) {
-            form.addEventListener('submit', function (event) {
-                if (!form.checkValidity()) {
-                event.preventDefault()
-                event.stopPropagation()
-            }
-            form.classList.add('was-validated')
-        }, false)
-    })
-})()
+//Asignando el usuario al input del login
+document.getElementById('btn-mali').onclick = function(){
+    document.getElementById('user').value = 'Mali';
+}
+document.getElementById('btn-gera').onclick = function(){
+    document.getElementById('user').value = 'Gera';
+}
+document.getElementById('btn-maui').onclick = function(){
+    document.getElementById('user').value = 'Maui';
+}
+
+//Validar cuenta y contraseña
+function validatePassword(){
+    let button = document.getElementById('btn-login');
+    if(document.getElementById('accountCheck').checked){
+        button.disabled = false;
+    }else if(!document.getElementById('accountCheck').checked){
+        button.disabled = true;
+    }else{
+        console.log('fallo');
+    }
+    
+}
