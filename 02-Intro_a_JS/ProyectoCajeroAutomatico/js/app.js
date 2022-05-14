fa1b05dffd85bf3a628323bd927424185f7ab5d5
Michelle Arellano <michelle.arellanofig@gmail.com>
2022-05-13 19:11:21 -0500

    Validando el inicio de sesión

--- 02-Intro_a_JS/ProyectoCajeroAutomatico/js/app.js
+++ 02-Intro_a_JS/ProyectoCajeroAutomatico/js/app.js
@@ -1,3 +1,7 @@
+//Variable global
+let accountSelected;
+let openLogin = 0;
+
 //Cuentas disponibles
 const accounts = [
     {name: "Mali", money: 200},
@@ -19,23 +23,68 @@ for(let i = 0; i < accounts.length; i++){
 //Asignando el usuario al input del login
 document.getElementById('btn-mali').onclick = function(){
     document.getElementById('user').value = 'Mali';
+    accountSelected = 'Mali';
 }
 document.getElementById('btn-gera').onclick = function(){
     document.getElementById('user').value = 'Gera';
+    accountSelected = 'Gera';
 }
 document.getElementById('btn-maui').onclick = function(){
     document.getElementById('user').value = 'Maui';
+    accountSelected = 'Maui';
 }
 
+//Revisar lo de las opciones de login sin pass y checke
 //Validar cuenta y contraseña
-function validatePassword(){
-    let button = document.getElementById('btn-login');
-    if(document.getElementById('accountCheck').checked){
-        button.disabled = false;
-    }else if(!document.getElementById('accountCheck').checked){
-        button.disabled = true;
-    }else{
-        console.log('fallo');
+function validateAccount(){
+    let passwordAccount = document.getElementById('password').value;
+    let buttonLogin = document.getElementById('btn-login');
+    let checkBoxAccount = document.getElementById('accountCheck');
+
+
+    if(passwordAccount=== "" && !checkBoxAccount.checked){
+        checkBoxAccount.disabled = true;
+        checkBoxAccount.checked = 0;
+    }else if(passwordAccount === "" && checkBoxAccount.checked){
+        checkBoxAccount.disabled = true;
+        checkBoxAccount.checked = 0;
     }
-    
+
+    if(passwordAccount === ""){
+        console.log('ingresa una contraseña');
+        buttonLogin.disabled = true;
+        checkBoxAccount.disabled = true;
+    }else{
+        checkBoxAccount.disabled = false;
+        if(checkBoxAccount.checked){
+            passwordAccount.disabled = true;
+            buttonLogin.disabled = validatePassword(passwordAccount);
+        }else if(!checkBoxAccount.checked){
+            buttonLogin.disabled = true;
+            passwordAccount.disabled = false;
+        }else{
+            console.log('fallo');
+        }
+    }
+}
+
+//Validar contraseña
+function validatePassword(pass){
+    let passMali = '@Mali123';
+    let passGera = '@Gera123';
+    let passMaui = '@Maui123';
+
+    if(accountSelected === 'Mali' && pass === passMali){
+        console.log('Bienvenida Mali');
+        return false;
+    }else if(accountSelected === 'Gera' && pass === passGera){
+        console.log('Bienvenida Gera');
+        return false;
+    }else if(accountSelected === 'Maui' && pass === passMaui){
+        console.log('Bienvenido Maui');
+        return false;
+    }else{
+        openLogin = 0;
+        return true;
+    }
 }
