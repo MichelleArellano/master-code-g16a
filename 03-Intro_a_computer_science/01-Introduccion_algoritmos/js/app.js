bc1e772dd0b1d5adb31b452d1582d089f76cee65
Michelle Arellano <michelle.arellanofig@gmail.com>
2022-06-10 18:14:53 -0500

    Introduccion a algoritmos

---
+++ 03-Intro_a_computer_science/01-Introduccion_algoritmos/js/app.js
@@ -0,0 +1,17 @@
+/*
+    Pseudocodigo
+*/
+
+/*
+var Ingredientes = [Harina, Huevos, Azucar, Leche, Mantequiella, Polvo, Platanos]
+var Horno
+var Molde
+var Batidora
+
+metd hacer_pastel
+    precalentar_horno(250)
+    mezcla = mezclar(Ingredientes)
+    molde_mezcla = Molde(mezcla)
+    Horno(molde_mezcla, 30)
+end hacer_pastel
+*/
