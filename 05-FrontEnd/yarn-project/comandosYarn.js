e4d2cc5aec0df659ea30e6856abf7472c648b877
Michelle Arellano <michelle.arellanofig@gmail.com>
2022-09-05 12:40:28 -0500

    yarn project

---
+++ 05-FrontEnd/yarn-project/comandosYarn.js
@@ -0,0 +1,15 @@
+/**
+ * 
+ * Instalar paquetes
+ * 
+ * download dependencies - yarn
+ * 
+ * install - yarn add package_name
+ * 
+ * install dev dependencies - yarn add --dev package_name
+ * 
+ * uninstall package - yarn remove package_name
+ * 
+ * update - yarn upgrade
+ * 
+ */
